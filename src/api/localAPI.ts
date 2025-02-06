ee61f6cbfe9e76a5dc8ed785297bc1c6f9f398c8
Luis Jose Medina <luis06jose@gmail.com>
2025-02-05 23:33:56 -0400

    Convertir archivos JavaScript a TypeScript
    Implementación de tipos, adaptación y reemplazo de los archivos
    JavaScript en la carpeta `api` por sus equivalentes en TypeScript. Esto
    mejora la mantenibilidad y escalabilidad del código, además de prevenir
    posibles errores gracias al tipado estático.

--- src/api/localAPI.ts
+++ src/api/localAPI.ts
@@ -1,14 +1,19 @@
 import { jwtDecode } from 'jwt-decode';
 
+interface TokenData {
+  exp: number; // Expiration time in seconds
+  sub: string; // Subject (user ID)
+}
+
 const localAPI = {
-  setToken: (token) => localStorage.setItem('token', token),
+  setToken: (token: string) => localStorage.setItem('token', token),
   getToken: () => localStorage.getItem('token'),
   getTokenData() {
     const token = localStorage.getItem('token');
     let tokenDecoded = null;
 
     if (token) {
-      tokenDecoded = jwtDecode(token);
+      tokenDecoded = jwtDecode<TokenData>(token);
     }
     return tokenDecoded;
   },
